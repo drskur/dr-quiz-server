5f4e3bed86855dd2e16ea3e2b9c7321b97b7c812
윤창헌 <drskur@me.com>
2021-09-05 17:03:26 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,8 @@ pub mod models;
 pub mod filters;
 
 pub async fn get_mongodb_client() -> mongodb::error::Result<Client> {
-    let opt = ClientOptions::parse("mongodb://drskur:hNkEVxtL0hzrSukhZnNXktU4lmlzWFtoWs2MlGykDhdnQlMfEv9AYxwbeYrDDDlx5DgxqePR1Cx8Xyy5zNYh3A==@drskur.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@drskur@")
+    let mongo_url = std::env::var("MONGO_URL").expect("MONGO_URL must be set");
+    let opt = ClientOptions::parse(&mongo_url)
         .await?;
     let client = Client::with_options(opt)?;
 
